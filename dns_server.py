d507bee480898bee611e6672d520b6a06d48e3bb
Alysson93 <alyssonpereiraassuncao@gmail.com>
2023-07-06 17:15:16 -0300

    threads in dns

--- dns_server.py
+++ dns_server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 domain_mappings = {
 	'exemplo.com': ('127.0.0.1', 6000),
@@ -8,6 +9,22 @@ domain_mappings = {
 ip = '127.0.0.1'
 port = 5000
 
+##############################
+
+def handle_request(data, client):
+	url = data.decode()
+
+	if url in domain_mappings:
+		address = domain_mappings[url]
+		response = f'{address[0]}:{address[1]}'
+	else:
+		response = 'no url'
+	
+	sock.sendto(response.encode(), client)
+
+
+##############################
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((ip, port))
 
@@ -16,13 +33,5 @@ print(f'DNS server is running on {ip}:{port}')
 
 while True:
 	data, client = sock.recvfrom(1024)
-	url = data.decode()
-
-	if url in domain_mappings:
-		address = domain_mappings[url]
-		response = f'{address[0]}:{address[1]}'
-	else:
-		response = 'no url'
-
-	
-	sock.sendto(response.encode(), client)
+	thread = threading.Thread(target=handle_request, args=(data, client))
+	thread.start()
